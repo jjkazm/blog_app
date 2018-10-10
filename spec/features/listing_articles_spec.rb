aad60c7865bd4dec5ee8d55a5398c59a9e4c77f3
jjkazm <jjkazm@gmail.com>
2018-10-10 13:43:26 +0200

    add no_articles scenario

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -15,4 +15,22 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article2.title)
   end
+
+  scenario "List article, when no articles present" do
+    Article.delete_all
+
+    visit "/"
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article2.title)
+
+    within("h1#no-articles") do
+      expect(page).to have_content("No articles yet")
+    end
+  end
+
+
 end
