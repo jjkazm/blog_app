a9c7505da47e13b7312680b84084e0dd3d646d1d
jjkazm <jjkazm@gmail.com>
2018-10-12 14:11:51 +0200

    restrict editing and deleting articles for non-owners users

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,16 +3,43 @@ require "rails_helper"
 RSpec.feature "Showing article" do
   before do
     @adam = User.create(email: "adam@example.com", password: "haslo1")
-    login_as(@adam)
+    @jurek = User.create(email: "jurek@example.com", password: "haslo1")
+
     @article = Article.create(title: "This is first article", body: "Ram pam pam", user: @adam)
   end
-  scenario "A user shows an article" do
+  scenario "A user shows an article when user is owner of the article" do
+    login_as(@adam)
     visit "/"
     click_link @article.title
 
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
+  end
+
+  scenario "A user shows an article when user is owner of the article" do
+    login_as(@jurek)
+    visit "/"
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "A user shows an article when visitor is NOT signed in" do
+    visit "/"
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
   end
 
 end
